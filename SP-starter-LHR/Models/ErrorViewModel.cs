6f24ad49da6848da5298fa0d8c9ed1a3e2130864
contosoTecnomics@hotmail.com <contosoTecnomics@hotmail.com>
2019-05-01 11:59:45 +0500

    Add project files.

---
+++ SP-starter-LHR/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SP_starter_LHR.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
